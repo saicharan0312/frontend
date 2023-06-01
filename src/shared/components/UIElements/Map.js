10df2de1a40e6f34a569102e137e92955a84fa2e
saicharan0312 <saicharan0312@gmail.com>
2023-06-01 16:51:30 -0400

    last commit

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 import './Map.css';
 
@@ -6,19 +7,28 @@ const Map = props => {
   const mapRef = useRef();
   
   const { center, zoom } = props;
-
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-7.9);
+  const [lat, setLat] = useState(20.35);
+  const [zoom1, setZoom] = useState(9);
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic2FpY2hhcmFuMDMxMiIsImEiOiJjbGk4OHBzYzgxN251M2VtOTJxMmZ5dW9sIn0.NyZxVDLI8vWTzKX5E9JPjg';
   useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom
+ 
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: props.center,
+    zoom: props.zoom,
     });
-  
-    new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+
+    }, [center, zoom]
+  );  
 
   return (
     <div
-      ref={mapRef}
+      ref={mapContainer}
       className={`map ${props.className}`}
       style={props.style}
     ></div>
